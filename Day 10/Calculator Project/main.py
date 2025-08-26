1d6e6ffe1f376bb7c195cd22ad0343e211142673
seeker8 <chuyo_gk87@yahoo.com.mx>
2025-08-26 14:12:42 -0400

    day 10 - add substract, multiply and divide

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,6 +1,19 @@
 from art import logo
 print(logo)
+
 def add(n1, n2):
+    """
+    Add two numbers
+    """
     return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
 
 
