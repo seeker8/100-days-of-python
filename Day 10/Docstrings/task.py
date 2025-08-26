aebf4633914847ddf7cddc945be1addbe34b6910
seeker8 <chuyo_gk87@yahoo.com.mx>
2025-08-26 14:03:35 -0400

    add docstrings

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,4 +1,11 @@
 def format_name(f_name, l_name):
+    """
+    Take a fist and last name and format it to return
+    the title case version of the name.
+    :param f_name: first name
+    :param l_name: last name
+    :return: formatted first and last name Firstname Lastname
+    """
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
