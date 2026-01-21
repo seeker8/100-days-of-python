ee23a387a184219cdfcb68370b67a7821ec246f2
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-01-20 22:28:44 -0500

    update pending logic for blackjack game and remove unused code

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -15,36 +15,13 @@ def print_hands(score, hand, computer_hand):
     print(computer_message.format(computer_cards=computer_hand))
 
 def print_final_hands(score, c_score, hand, computer_hand):
-    final_score_message = "   Your final hand: [{user_cards}], final score: {user_score}"
+    final_score_message = "   Your final hand: {user_cards}, final score: {user_score}"
     final_computer_message = "   Computer's final hand: {computer_cards}, final score: {computer_score}"
     print(final_score_message.format(user_score=score, user_cards=hand))
     print(final_computer_message.format(computer_cards=computer_hand, computer_score=c_score))
 
 def get_score(hand):
     return reduce(lambda x, y: x + y, hand)
-
-# def deal_cards(user_cards, computer_cards):
-#     user_decision = input(prompt_message)
-#     while(user_decision == "y" or user_score <21):
-#         user_cards.append(random.choice(cards))
-#         user_score = reduce(lambda x, y: x + y, user_cards)
-#         print_hands()
-#         if user_score < 21:
-#             user_decision = input(prompt_continue)
-#         else:
-#             break
-#
-#     if user_score > 21:
-#         print("You went over. You lose")
-
-def play():
-    ...
-
-# ask if user wants to play blackjack
-# yes
-    # print logo
-    # print hands(user n computer)
-    # ask user if they want another card
 
 if __name__ == "__main__":
     start_game_prompt = "Do you want to play a game of Balckjack? Type 'y' or 'n': "
@@ -57,19 +34,34 @@ if __name__ == "__main__":
         print(logo)
         print_hands(user_score, user_cards, computer_cards)
         keep_playing = 'y'
-        while user_score < 21:
+        while user_score < 21 and keep_playing == 'y':
             keep_playing = input(prompt_message)
             if keep_playing == "y":
                 user_cards.append(random.choice(cards))
                 user_score = get_score(user_cards)
                 print_hands(user_score, user_cards, computer_cards)
             else:
-                break
+                while 18 > computer_score < 21:
+                    computer_cards.append(random.choice(cards))
+                    computer_score = get_score(computer_cards)
+
         if user_score == 21:
             print_final_hands(user_score, computer_score, user_cards, computer_cards)
             print("Win with a Blackjack!")
             start_game = input(start_game_prompt)
+        elif computer_score > 21:
+            print_final_hands(user_score, computer_score, computer_cards, computer_cards)
+            print("Opponent went over. You win")
+            start_game = input(start_game_prompt)
         elif user_score > 21:
             print_final_hands(user_score,computer_score, user_cards, computer_cards)
             print("You went over. You lose")
+            start_game = input(start_game_prompt)
+        elif 21 - user_score < 21 - computer_score:
+            print_final_hands(user_score, computer_score, user_cards, computer_cards)
+            print("You win.")
+            start_game = input(start_game_prompt)
+        else:
+            print_final_hands(user_score, computer_score, user_cards, computer_cards)
+            print("You lose.")
             start_game = input(start_game_prompt)
