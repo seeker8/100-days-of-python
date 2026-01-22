d45d4a0f02b834f645375d7cee846ce72efe258d
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-01-22 15:04:49 -0500

    debuging analysis

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -8,5 +8,8 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+        # iterates the values from 1 to 20 (not inclusive)
 # 2. When is the function meant to print "You got it"?
+        # when i is equal to 20
 # 3. What are your assumptions about the value of i?
+        # i never gets to 20, becuase the end value in range is not inclusive
